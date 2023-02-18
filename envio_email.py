d3e6dd09bdef780bfe90a3e18fd854c468397684
diogoheck <diogo.heck.dev@gmail.com>
2023-02-18 18:33:21 -0300

    versao_3

--- envio_email.py
+++ envio_email.py
@@ -4,22 +4,23 @@ import mimetypes
 import os
 
 
-def enviar_email():
+def enviar_email(dir_raiz):
     smtpsrv = "smtp.office365.com"
     smtpserver = smtplib.SMTP(smtpsrv, 587)
 
     msg = EmailMessage()
 
     # textos do email
-    assunto = 'Log Limpeza do rede da Pasta Trabalhista'
-    body = open('corpo_email.txt', 'r', encoding='utf-8').read()
+    assunto = 'MENSAGEM AUTOMÁTICA -> Log Limpeza do rede da Pasta Trabalhista'
+    body = open(dir_raiz + os.sep + 'corpo_email.txt',
+                'r', encoding='utf-8').read()
 
     msg['Subject'] = assunto
     msg['From'] = 'diogo.rodrigues@supervisao.com.vc'
     msg['To'] = ('diogoheck6@gmail.com', 'diogo.rodrigues@supervisao.com.vc')
 
     # estruturação de um arquivo anexo
-    anexo_path = 'log_limpeza_u_dp.txt'
+    anexo_path = dir_raiz + os.sep + 'log_limpeza_u_dp.txt'
     anexo_arquivo = os.path.basename(anexo_path)
     mime_type, _ = mimetypes.guess_type(anexo_path)
     mime_type, mime_subtype = mime_type.split('/', 1)
--- limpeza_rede.py
+++ limpeza_rede.py
@@ -6,34 +6,34 @@ lista_proibidos = ['Trabalhista']
 pasta_sincronizacao = ['.sync']
 
 
-def salvar_logs(conteudo):
-    with open(os.getcwd() + os.sep + 'log_limpeza_u_dp.txt', 'a') as log:
+def salvar_logs(conteudo, dir_raiz):
+    with open(dir_raiz + os.sep + 'log_limpeza_u_dp.txt', 'a') as log:
         print(conteudo, file=log)
 
 
-def remover_arquivo(arquivo):
+def remover_arquivo(arquivo, dir_raiz):
     try:
         os.remove(arquivo)
-        salvar_logs(f'{arquivo} removido com sucesso')
+        salvar_logs(f'{arquivo} removido com sucesso', dir_raiz)
     except Exception as E:
         salvar_logs(E)
 
 
-def limpeza_unico_pastas():
+def limpeza_unico_pastas(dir_raiz):
 
     folder_list = []
 
     os.chdir('U:\\')
     cwd = os.getcwd()
 
-    if os.path.exists('log_limpeza_u_dp.txt'):
-        os.remove('log_limpeza_u_dp.txt')
+    if os.path.exists(dir_raiz + os.sep + 'log_limpeza_u_dp.txt'):
+        os.remove(dir_raiz + os.sep + 'log_limpeza_u_dp.txt')
 
     for i in os.listdir(cwd):
         if os.path.isdir(i) and i in lista_proibidos and i not in pasta_sincronizacao:
             folder_list.append(i)
         elif i in lista_proibidos and i not in pasta_sincronizacao and '.py' not in i:
-            remover_arquivo(i)
+            remover_arquivo(i, dir_raiz)
             pass
 
     for folder_raiz in folder_list:
@@ -41,7 +41,7 @@ def limpeza_unico_pastas():
             path2 = cwd + '\\' + folder_raiz
             os.chdir(path2)
             sub_folder_list = [i if os.path.isdir(
-                i) else remover_arquivo(i) for i in os.listdir(path2)]
+                i) else remover_arquivo(i, dir_raiz) for i in os.listdir(path2)]
 
         for subfolder in sub_folder_list:
             if subfolder:
@@ -51,17 +51,20 @@ def limpeza_unico_pastas():
                     if folder2:
                         try:
                             if os.path.isfile(folder2):
-                                remover_arquivo(folder2)
+                                remover_arquivo(folder2, dir_raiz)
                             else:
                                 shutil.rmtree(folder2)
+                                salvar_logs(
+                                    f'{folder2} removido com sucesso', dir_raiz)
                         except PermissionError:
                             pass
                         except:
                             pass
 
-    salvar_logs('limpeza efetuada com sucesso!!')
+    salvar_logs('limpeza efetuada com sucesso!!', dir_raiz)
 
 
 if __name__ == '__main__':
-    limpeza_unico_pastas()
-    enviar_email()
+    dir_raiz = os.getcwd()
+    limpeza_unico_pastas(dir_raiz)
+    enviar_email(dir_raiz)
